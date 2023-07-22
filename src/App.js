eb38824bb2b3a62d9651e19d7dd80c2201f6220e
sydmasina <masinasydwell@gmail.com>
2023-07-22 12:32:13 +0200

    Remove unrequired default react styling

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ import TodoList from './Components/TodoList/TodoList';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-       <TodoList />
-      </header>
+      <TodoList />
     </div>
   );
 }
