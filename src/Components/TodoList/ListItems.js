4d27cdbf4e4118dd7572829ee90ac2b36a888a10
sydmasina <masinasydwell@gmail.com>
2023-07-22 12:33:28 +0200

    Add handleEdit function and delete btn className

--- src/Components/TodoList/ListItems.js
+++ src/Components/TodoList/ListItems.js
@@ -3,12 +3,20 @@ import React, { Component } from 'react';
 class ListItems extends Component{
     constructor(props){
         super(props);
+        this.state = {content: ""}
+    }
+    handleEdit(item){
+        return(
+            <input value={item} name='content' />
+        )
     }
     render(){
         return(
             <ul>
                 {this.props.items.map(item=>{
-                    return <li key={item.id}>{item.content}    <button onClick={()=> this.props.removeItem(item.id)}>X</button></li>
+                    return <li key={item.id}>{item.content}    
+                     <button className='todo-card-delete-btn' onClick={()=> this.props.removeItem(item.id)}>X</button>
+                    </li>
                 })}
             </ul>
         )
